5556b518c4dd90fb8566d3a432bbd0f530a3edef
Amreen Shaikh <amreen.shaikh.21.05@gmail.com>
2019-07-09 14:47:48 +0530

    Amreen1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
 
+    public  static void hello()
+    {
+        System.out.println("This is Amreen.");
+        System.out.println("I am trying to make some changes here.");
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
         System.out.println("I hope this gets accepted to master.");
+        Main.hello();
+
     }
 }// i am editing this file - shraddha
 
