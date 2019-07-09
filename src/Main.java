1861a6a64e5932b7a47866b60d6476c0ca2a6a42
Vigneshwaran Dharmalingam <vigneshwaran2121997@gmail.com>
2019-07-09 12:48:12 +0530

    Cars is added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        Cars car = new Cars(1,"BMW");
+        System.out.println(car.toString());
     }
 }// i am editing this file - shraddha
 
